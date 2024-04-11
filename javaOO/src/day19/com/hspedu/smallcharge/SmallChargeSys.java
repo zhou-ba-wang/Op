b947cd1ea2225979b469e9113b602f2060ef5558
King <3429317309@qq.com>
2024-04-11 11:31:25 +0800

    第二次提交

--- javaOO/src/day19/com/hspedu/smallcharge/SmallChargeSys.java
+++ javaOO/src/day19/com/hspedu/smallcharge/SmallChargeSys.java
@@ -17,17 +17,24 @@ public class SmallChargeSys {
         do {
             smallChargeSys.MainOrder();
             Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入你的选择（1-4）");
             choice = scanner.nextInt();
+
             switch (choice) {
                 case 1:
+                    System.out.println("1.零钱通明细 ");
                     break;
                 case 2:
+                    System.out.println("2.消费入账");
                     break;
                 case 3:
+                    System.out.println(" 3.消费");
                     break;
                 case 4:
+                    System.out.println(" 4.退    出 ");
                     break;
                 default:
+                    System.out.println("错误选择");
                     break;
             }
 
